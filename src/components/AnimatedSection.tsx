ae8a131db7dd899d613a40093fba82eca0668942
Vishwajeet Raj <ext-vishwajeet.raj@graphy.com>
2023-10-27 15:03:26 +0530

    fixes

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Down from "./svgs/Down";
-import { motion } from "framer-motion";
+import { motion, useAnimation, useInView } from "framer-motion";
 type Props = {};
 
 const nodeArr = [
@@ -30,15 +30,31 @@ const nodeArr = [
 ];
 
 const AnimatedSection = (props: Props) => {
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const inView = useInView(sectionRef, { once: true });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      mainControls.start("initial");
+    }
+  }, [inView]);
+
   return (
-    <section>
+    <motion.div ref={sectionRef}>
       <div className="w-full max-w-7xl mx-auto px-6 md:px-4">
         <div className="flex flex-col justify-center md:text-center text-[28px] md:text-[40px] leading-[34px] md:leading-[1.2] tx-black-primary md:pb-16 font-medium">
           {nodeArr.map((node, i) => (
             <motion.p
               key={i}
+              variants={{
+                initial: { opacity: 1, translateY: 0 },
+              }}
+              animate={mainControls}
               initial={{ opacity: 0, translateY: 50 }}
-              animate={{ opacity: 1, translateY: 0 }}
+              // animate={{ opacity: 1, translateY: 0 }}
               transition={{ duration: 0.7, delay: i * 0.5 }}
             >
               {node}
@@ -47,8 +63,12 @@ const AnimatedSection = (props: Props) => {
         </div>
         <div className="flex md:justify-center py-12">
           <motion.div
+            variants={{
+              initial: { opacity: 1, translateY: 0 },
+            }}
             initial={{ opacity: 0, translateY: -50 }}
-            animate={{ opacity: 1, translateY: 0 }}
+            // animate={{ opacity: 1, translateY: 0 }}
+            animate={mainControls}
             transition={{ duration: 0.7, delay: 0.7 * 3 }}
             style={{
               background:
@@ -60,7 +80,7 @@ const AnimatedSection = (props: Props) => {
           </motion.div>
         </div>
       </div>
-    </section>
+    </motion.div>
   );
 };
 
